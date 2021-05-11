6954ba60ac0c249b8c843f2dfca7e607d518417f
Courtney Brown <courtneybrown@Courtneys-MacBook-Pro.local>
2021-05-11 09:52:27 -0500

    added routes to create employee and change role

--- routes/apiRoutes/employeeRoutes.js
+++ routes/apiRoutes/employeeRoutes.js
@@ -18,4 +18,45 @@ router.get('/employees', (req, res) => {
     });
   });
 
+// Add an employee
+router.post('/employee', ({ body }, res) => {
+    const sql = `INSERT INTO employee (first_name, last_name, role_id, manager_id)
+        VALUES (?,?,?,?)`;
+    const params = [body.first_name, body.last_name, body.role_id, body.manager_id];
+
+    db.query(sql, params, (err, result) => {
+        if (err) {
+            res.status(400).json({ error: err.message });
+            return;
+        }
+        res.json({
+            message: 'success',
+            data: body
+        });
+    });
+  });
+
+// Update an employee's role
+router.put('/employee/:id', (req, res) => {
+    const sql = `UPDATE employee SET role_id = ? 
+                 WHERE id = ?`;
+    const params = [req.body.role_id, req.params.id];
+    db.query(sql, params, (err, result) => {
+      if (err) {
+        res.status(400).json({ error: err.message });
+        // check if a record was found
+      } else if (!result.affectedRows) {
+        res.json({
+          message: 'Employee not found'
+        });
+      } else {
+        res.json({
+          message: 'success',
+          data: req.body,
+          changes: result.affectedRows
+        });
+      }
+    });
+  });
+
 module.exports = router;
