0ad0fbb7bbe439726eb70b6bc309d492f3bc5b53
Courtney Brown <courtneybrown@Courtneys-MacBook-Pro.local>
2021-05-11 09:43:52 -0500

    add a role route complete

--- routes/apiRoutes/roleRoutes.js
+++ routes/apiRoutes/roleRoutes.js
@@ -17,4 +17,22 @@ router.get('/roles', (req, res) => {
     });
   });
 
+// Add a role
+router.post('/role', ({ body }, res) => {
+    const sql = `INSERT INTO role (title, salary, department_id)
+        VALUES (?,?,?)`;
+    const params = [body.title, body.salary, body.department_id];
+
+    db.query(sql, params, (err, result) => {
+        if (err) {
+            res.status(400).json({ error: err.message });
+            return;
+        }
+        res.json({
+            message: 'success',
+            data: body
+        });
+    });
+  });
+
 module.exports = router;
