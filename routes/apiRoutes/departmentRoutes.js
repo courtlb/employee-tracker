f41e8234e8c56b364e92a172ad3135428e48500c
Courtney Brown <courtneybrown@Courtneys-MacBook-Pro.local>
2021-05-11 09:39:24 -0500

    add a department route complete

--- routes/apiRoutes/departmentRoutes.js
+++ routes/apiRoutes/departmentRoutes.js
@@ -17,5 +17,23 @@ router.get('/departments', (req, res) => {
     });
   });
 
+// Add a department
+router.post('/department', ({ body }, res) => {
+    const sql = `INSERT INTO department (name)
+        VALUES (?)`;
+    const params = [body.name];
+
+    db.query(sql, params, (err, result) => {
+        if (err) {
+            res.status(400).json({ error: err.message });
+            return;
+        }
+        res.json({
+            message: 'success',
+            data: body
+        });
+    });
+  });
+
 
 module.exports = router;
